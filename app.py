3fe774472250a4a181490f68bdb8512c810571d3
Goodreach I Chavula <goodreachchavula@gmail.com>
2026-04-05 14:21:19 +0200

    Edit

--- app.py
+++ app.py
@@ -284,6 +284,7 @@ def add_payment(loan_id):
 # -------------------
 # RUN APP
 # -------------------
+init_db()
+
 if __name__ == "__main__":
-    init_db()
     app.run(host='0.0.0.0', port=10000)
